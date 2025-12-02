ae214b3f738adf5c629309f67559d705ea4ded3e
Vrishty Gothwal <gothwalvrishty@gmail.com>
2025-12-02 11:02:17 +0530

    logging and exception

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,6 @@
+'''
+Data ingestion is the process of collecting and importing raw data 
+from various sources into a centralized storage system
+(like a data warehouse, data lake, or database) where it can be stored, 
+processed, and analyzed.
+'''
---
+++ src/components/data_transformation.py
@@ -0,0 +1,6 @@
+'''
+Data transformation is the process of converting raw data from one format, 
+structure, or value into another to make 
+it suitable for a specific purpose, such as data analysis,
+data warehousing, or machine learning.
+'''
